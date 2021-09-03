06f0fe3fa79eb35508d00ef1307fdc3c74260dc0
Daniel Parangi <dmp267@cornell.edu>
2021-09-03 13:16:21 -0400

    rename while working on full adapter

--- adapter.py
+++ adapter.py
@@ -1,22 +1,19 @@
-# Shallow adapter over dWeather APIv3 endpoint
-import os
+#Adapter that clones dClimate client functionality for grid cell history retrieval
 
-from bridge import Bridge
-from tools import PARAM_SETTERS
-
-AUTH_TOKEN = os.environ['AUTH_TOKEN']
+# from bridge import Bridge
+from program_catalog_stub.fake_program_catalog.programs.cambodia_rainfall import Contract, CambodiaRainfall
+from tools import CONFIGS
 
 class Adapter:
+
     def __init__(self, input):
-        self.base_url = 'https://api.dclimate.net/apiv3'
-        self.params = {}
-        self.headers = {'Authorization': AUTH_TOKEN, 'accept': 'application/json'}
         self.id = input.get('id', '1')
         self.request_data = input.get('data')
         if self.validate_request_data():
-            self.bridge = Bridge()
-            self.set_params()
-            self.create_request()
+            self.execute_request()
+            # self.bridge = Bridge()
+            # self.set_params()
+            # self.create_request()
         else:
             self.result_error('No data provided')
 
@@ -27,23 +24,43 @@ class Adapter:
             return False
         return True
 
-    def set_params(self):
-        job_type = self.request_data.get('job_type')
-        param_setter = PARAM_SETTERS[job_type]
-        endpoint, params = param_setter(self.request_data, job_type)
-        self.base_url = os.path.join(self.base_url, endpoint)
-        self.params = params
-
-    def create_request(self):
+    def execute_request(self):
         try:
-            response = self.bridge.request(self.base_url, params=self.params, headers=self.headers)
-            data = response.json()
-            self.result = data
+            task = self.request_data.get('task')
+            getter = CONFIGS[task]['getter']
+            data = getter(self.request_data)
+            result = do_something(data)
             self.result_success(data)
         except Exception as e:
             self.result_error(e)
         finally:
             self.bridge.close()
+
+    # def set_params(self):
+    #     for param in self.from_params:
+    #         self.from_param = self.request_data.get(param)
+    #         if self.from_param is not None:
+    #             break
+    #     for param in self.to_params:
+    #         self.to_param = self.request_data.get(param)
+    #         if self.to_param is not None:
+    #             break
+    #
+    # def create_request(self):
+    #     try:
+    #         params = {
+    #             'fsym': self.from_param,
+    #             'tsyms': self.to_param,
+    #         }
+    #         response = self.bridge.request(self.base_url, params)
+    #         data = response.json()
+    #         self.result = data[self.to_param]
+    #         data['result'] = self.result
+    #         self.result_success(data)
+    #     except Exception as e:
+    #         self.result_error(e)
+    #     finally:
+    #         self.bridge.close()
 
     def result_success(self, data):
         self.result = {
@@ -60,8 +77,3 @@ class Adapter:
             'error': f'There was an error: {error}',
             'statusCode': 500,
         }
-
-#pipenv install
-#pipenv run python app.py
-#curl -X POST -H "content-type:application/json" "http://0.0.0.0:8080/" --data '{ "id": 0, "data": {"job_type": "Dataset Information", "dataset": "cpcc_precip_us-daily"} }'
-#curl -X POST -H "content-type:application/json" "http://0.0.0.0:8080/" --data '{ "id": 0, "data": {"job_type": "Grid File Dataset History", "dataset": "cpcc_precip_us-daily", "lat": 40, "lon": -100} }'

6d2ebbebe3ca54cb55bc5923eb4f540850360aed
Daniel Parangi <dmp267@cornell.edu>
2021-09-07 21:26:45 -0400

    updating readme

--- adapter.py
+++ adapter.py
@@ -80,5 +80,5 @@ class Adapter:
 # }
 # pipenv install
 # pipenv run python app.py
-# curl -X POST -H "content-type:application/json" "http://0.0.0.0:8080/" --data '{ "id": 0, "data": {"program": "cambodia_rainfall", "task": "serve_contract", "params": {"dataset": "chirpsc_final_05-daily", "lat": 100.0, "lon": -95.0, "optional_params": {"also_return_snapped_coordinates": "True"}, "task_params": {"start": "2021-08-01", "end": "2021-08-31", "strike": 0.5, "exhaust": 0.25, "limit": 1000, "option_type": "PUT"} } } }'
-# curl -X POST -H "content-type:application/json" "http://0.0.0.0:8080/" --data '{"id": 1, "data": {"program": "cambodia_rainfall", "task": "serve_evaluation", "params": {"these dont do anything": 0, "these dont do anything_": 1, "these dont do anything__": 2} } }'
+# curl -X POST -H "content-type:application/json" "http://0.0.0.0:8080/" --data '{ "id": 0, "data": { "program": "cambodia_rainfall", "task": "serve_contract", "params": { "dataset": "chirpsc_final_05-daily", "lat": 100.0, "lon": -95.0, "optional_params": { "also_return_snapped_coordinates": "True" }, "task_params": { "start": "2021-08-01", "end": "2021-08-31", "strike": 0.5, "exhaust": 0.25, "limit": 1000, "option_type": "PUT" } } } }'
+# curl -X POST -H "content-type:application/json" "http://0.0.0.0:8080/" --data '{ "id": 1, "data": { "program": "cambodia_rainfall", "task": "serve_evaluation", "params": { "these dont do anything": 0, "these dont do anything_": 1, "these dont do anything__": 2 } } }'

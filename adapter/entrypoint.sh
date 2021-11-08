fd00312bb101d648df0e7014382fb5bde409eccb
Daniel Parangi <dmp267@cornell.edu>
2021-11-08 17:37:20 -0500

    networking, docker, sol testing fixes

--- adapter/entrypoint.sh
+++ adapter/entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-gunicorn --bind 0.0.0.0:8000 wsgi:app --log-level info
+gunicorn --worker-class gevent --workers=2 --bind 172.17.0.1:8000 wsgi:app --log-level info
--- contracts/TestAdapter.sol
+++ contracts/TestAdapter.sol
@@ -7,59 +7,87 @@ import "@chainlink/contracts/src/v0.8/ConfirmedOwner.sol";
 contract ClimateOptionTest is ChainlinkClient, ConfirmedOwner {
     using Chainlink for Chainlink.Request;
 
-    uint256 constant private oraclePaymentAmount = 1 * LINK_DIVISIBILITY;
-    mapping (address => uint) public oracleMap;
+    uint256 private constant oraclePaymentAmount = 1 * LINK_DIVISIBILITY;
+    mapping(address => uint256) public oracleMap;
     address[] public oracles;
     bytes32[] public jobIds;
     address public provider;
     bool public contractActive;
     bool public contractEvaluated;
-    
+
     string private dataset;
     string private opt_type;
     string[] private locations;
-    uint private start;
-    uint private end;
-    uint private strike;
-    uint private limit;
-    uint private exhaust;
+    uint256 private start;
+    uint256 private end;
+    uint256 private strike;
+    uint256 private limit;
+    uint256 private exhaust;
     uint256 private payout;
-    
+
     /**
      * @dev Prevents a function being run unless the end date has been passed
      */
     modifier onContractEnded() {
-        require(end < block.timestamp, 'cannot call until coverage period has ended');
-          _;
+        require(
+            end < block.timestamp,
+            "cannot call until coverage period has ended"
+        );
+        _;
     }
-    
+
     /**
      * @dev Prevents a function being run unless the contract has been evaluated
      */
     modifier onContractEvaluated() {
-        require(contractEvaluated, 'cannot call until coverade period has ended');
-          _;
+        require(
+            contractEvaluated,
+            "cannot call until coverade period has ended"
+        );
+        _;
     }
-    
-    event contractCreated(address _contract, uint _start, uint _end, uint _limit);
-    event contractEnded(address _contract, uint _end, uint _time);
-    event contractEvaluationRequested(address _contract, bytes32 _req, uint _time);
-    event contractEvaluationFulfilled(address _contract, bytes32 _req, uint _time, uint256 _payout);
+
+    event contractCreated(
+        address _contract,
+        uint256 _start,
+        uint256 _end,
+        uint256 _limit
+    );
+    event contractEnded(address _contract, uint256 _end, uint256 _time);
+    event contractEvaluationRequested(
+        address _contract,
+        bytes32 _req,
+        uint256 _time
+    );
+    event contractEvaluationFulfilled(
+        address _contract,
+        bytes32 _req,
+        uint256 _time,
+        uint256 _payout
+    );
 
     /**
      * @dev Creates a new test contract
      */
     constructor() ConfirmedOwner(msg.sender) {
-                    
         setPublicChainlinkToken();
-        
+
         oracles.push(0xe9d0d0332934c269132e53c03D3fD63EbA41aae0);
-        jobIds.push(stringToBytes32('255b4810914f4237877c6cc1ea6e5f64'));
-        
+        jobIds.push(stringToBytes32("7dba7fd6956941978d4d3c552ec41363"));
+
         provider = msg.sender;
-        dataset = 'chirpsc_final_25-daily';
-        opt_type = 'CALL';
-        locations = ["[12.76727009, 104.01941681]", "[12.76727009, 104.26941681]", "[13.01727009, 104.01941681]", "[13.01727009, 104.26941681]", "[13.26727009, 104.01941681]", "[13.26727009, 104.26941681]", "[13.51727009, 104.01941681]", "[13.51727009, 104.26941681]"];
+        dataset = "chirpsc_final_25-daily";
+        opt_type = "CALL";
+        locations = [
+            "[12.76727009, 104.01941681]",
+            "[12.76727009, 104.26941681]",
+            "[13.01727009, 104.01941681]",
+            "[13.01727009, 104.26941681]",
+            "[13.26727009, 104.01941681]",
+            "[13.26727009, 104.26941681]",
+            "[13.51727009, 104.01941681]",
+            "[13.51727009, 104.26941681]"
+        ];
         start = 1627790400;
         end = 1632974400;
         strike = 60900;
@@ -69,39 +97,59 @@ contract ClimateOptionTest is ChainlinkClient, ConfirmedOwner {
         contractEvaluated = false;
         emit contractCreated(address(this), start, end, limit);
     }
-    
+
     /**
-      * @dev Makes a request to the oracle hosting the Arbol dApp external adapter and associated job
-      * to determine a contracts payout after the coverage period has ended
-      */
+     * @dev Makes a request to the oracle hosting the Arbol dApp external adapter and associated job
+     * to determine a contracts payout after the coverage period has ended
+     */
     function requestPayoutEvaluation() public onlyOwner onContractEnded {
         if (contractActive) {
             contractActive = false;
             emit contractEnded(address(this), end, block.timestamp);
-            Chainlink.Request memory req = buildChainlinkRequest(jobIds[0], address(this), this.fulfillPayoutEvaluation.selector);
-            req.add('dataset', dataset);
-            req.add('opt_type', opt_type);
-            req.addStringArray('locations', locations);
-            req.addUint('start', start);
-            req.addUint('end', end);
-            req.addUint('strike', strike);
-            req.addUint('limit', limit);
-            req.addUint('exhaust', exhaust);
-            bytes32 requestId = sendChainlinkRequestTo(oracles[0], req, oraclePaymentAmount);
-            emit contractEvaluationRequested(address(this), requestId, block.timestamp);   
+            Chainlink.Request memory req = buildChainlinkRequest(
+                jobIds[0],
+                address(this),
+                this.fulfillPayoutEvaluation.selector
+            );
+            req.add("dataset", dataset);
+            req.add("opt_type", opt_type);
+            req.addStringArray("locations", locations);
+            req.addUint("start", start);
+            req.addUint("end", end);
+            req.addUint("strike", strike);
+            req.addUint("limit", limit);
+            req.addUint("exhaust", exhaust);
+            bytes32 requestId = sendChainlinkRequestTo(
+                oracles[0],
+                req,
+                oraclePaymentAmount
+            );
+            emit contractEvaluationRequested(
+                address(this),
+                requestId,
+                block.timestamp
+            );
         }
     }
-    
-    function fulfillPayoutEvaluation(bytes32 _requestId, uint256 _result) public recordChainlinkFulfillment(_requestId) {
+
+    function fulfillPayoutEvaluation(bytes32 _requestId, uint256 _result)
+        public
+        recordChainlinkFulfillment(_requestId)
+    {
         payout = _result;
         contractEvaluated = true;
-        emit contractEvaluationFulfilled(address(this), _requestId, block.timestamp, _result);
+        emit contractEvaluationFulfilled(
+            address(this),
+            _requestId,
+            block.timestamp,
+            _result
+        );
     }
-    
+
     /**
      * @dev Get the payout value
      */
-    function getPayout() public view onContractEvaluated returns (uint) {
+    function getPayout() public view onContractEvaluated returns (uint256) {
         return payout;
     }
 
@@ -116,18 +164,18 @@ contract ClimateOptionTest is ChainlinkClient, ConfirmedOwner {
     /**
      * @dev Get the eth balance of the contract
      */
-    function getETHBalance() external view returns (uint) {
+    function getETHBalance() external view returns (uint256) {
         return address(this).balance;
     }
-    
+
     /**
      * @dev Get the link balance of the contract
      */
-    function getLINKBalance() external view returns (uint) {
+    function getLINKBalance() external view returns (uint256) {
         LinkTokenInterface link = LinkTokenInterface(getChainlinkToken());
         return link.balanceOf(address(this));
     }
-    
+
     /**
      * @dev add a new node and associated job ID to the contract evaluator set
      */
@@ -140,13 +188,13 @@ contract ClimateOptionTest is ChainlinkClient, ConfirmedOwner {
      * @dev remove a node and associated job ID from the contract evaluator set
      */
     function removeOracleJob(address oracle) external onlyOwner {
-        uint index = oracleMap[oracle];
-        oracles[index] = oracles[oracles.length-1];
+        uint256 index = oracleMap[oracle];
+        oracles[index] = oracles[oracles.length - 1];
         oracles.pop();
-        jobIds[index] = jobIds[jobIds.length-1];
+        jobIds[index] = jobIds[jobIds.length - 1];
         jobIds.pop();
     }
-    
+
     /**
      * @dev Get the dataset name
      */
@@ -160,7 +208,7 @@ contract ClimateOptionTest is ChainlinkClient, ConfirmedOwner {
     function getOptionType() external view returns (string memory) {
         return opt_type;
     }
-    
+
     /**
      * @dev Get the crop locations
      */
@@ -168,46 +216,45 @@ contract ClimateOptionTest is ChainlinkClient, ConfirmedOwner {
         return locations;
     }
 
-
     /**
      * @dev Get the contract start date
      */
-    function getStartDate() external view returns (uint) {
+    function getStartDate() external view returns (uint256) {
         return start;
     }
 
     /**
      * @dev Get the contract end date
      */
-    function getEndDate() external view returns (uint) {
+    function getEndDate() external view returns (uint256) {
         return end;
     }
 
     /**
      * @dev Get the contract strike
      */
-    function getStrike() external view returns (uint) {
+    function getStrike() external view returns (uint256) {
         return strike;
     }
 
     /**
      * @dev Get the contract limit
      */
-    function getLimit() external view returns (uint) {
+    function getLimit() external view returns (uint256) {
         return limit;
     }
 
     /**
      * @dev Get the contract exhaust
      */
-    function getExhaust() external view returns (uint) {
+    function getExhaust() external view returns (uint256) {
         return exhaust;
     }
 
     /**
      * @dev Get the current date/time according to the blockchain
      */
-    function getBlockTimeStamp() external view returns (uint) {
+    function getBlockTimeStamp() external view returns (uint256) {
         return block.timestamp;
     }
 
@@ -218,24 +265,32 @@ contract ClimateOptionTest is ChainlinkClient, ConfirmedOwner {
         return chainlinkTokenAddress();
     }
 
-    function stringToBytes32(string memory source) private pure returns (bytes32 result) {
+    function stringToBytes32(string memory source)
+        private
+        pure
+        returns (bytes32 result)
+    {
         bytes memory tempEmptyStringTest = bytes(source);
         if (tempEmptyStringTest.length == 0) {
             return 0x0;
         }
 
-        assembly { // solhint-disable-line no-inline-assembly
+        assembly {
+            // solhint-disable-line no-inline-assembly
             result := mload(add(source, 32))
         }
     }
-    
+
     /**
      * @dev Function to end provider contract, in case of bugs or needing to update logic etc,
      * funds are returned to the contract provider, including any remaining LINK tokens
      */
     function endContractInstance() external onlyOwner {
         LinkTokenInterface link = LinkTokenInterface(getChainlinkToken());
-        require(link.transfer(provider, link.balanceOf(address(this))), "Unable to transfer");
+        require(
+            link.transfer(provider, link.balanceOf(address(this))),
+            "Unable to transfer"
+        );
         selfdestruct(payable(provider));
     }
 }

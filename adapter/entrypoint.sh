92f6c51f92e0c419cbc4042121f1510c645495bd
Dan Parangi <dmp267@cornell.edu>
2021-11-17 14:10:41 -0500

    docker change

--- adapter/entrypoint.sh
+++ adapter/entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-gunicorn --worker-class gevent --bind 172.17.0.1:8000 wsgi:app --log-level info
+gunicorn --worker-class --workers=2 gevent --bind 172.17.0.1:8000 wsgi:app --log-level info

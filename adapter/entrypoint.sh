7bf5e757b2e840072d5ffd0545d65c4c0fcbe244
Dan Parangi <dmp267@cornell.edu>
2021-11-17 14:56:34 -0500

    typo fix

--- adapter/entrypoint.sh
+++ adapter/entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-
-gunicorn --worker-class gevent --workers 2 --bind 172.17.0.1:8000 wsgi:app --log-level info
+#172.17.0.1
+gunicorn --worker-class gevent --workers 2 --bind 0.0.0.0:8000 wsgi:app --log-level info

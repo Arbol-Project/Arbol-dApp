656f5bf7e2762b11fbe9a685e9e60a8d647dd03c
Dan Parangi <dmp267@cornell.edu>
2021-11-17 13:47:31 -0500

    attempted fix for edge case initial failure

--- adapter/entrypoint.sh
+++ adapter/entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-gunicorn --worker-class gevent --workers=2 --bind 172.17.0.1:8000 wsgi:app --log-level info
+gunicorn --worker-class gevent --bind 172.17.0.1:8000 wsgi:app --log-level info

c6a8d18c2e89e72c2bf0147c58e91173c2f3d717
Daniel Parangi <dmp267@cornell.edu>
2021-12-03 15:15:11 -0500

    i think its a stack size issue

--- adapter/program_catalog/programs/directory.py
+++ adapter/program_catalog/programs/directory.py
@@ -1,27 +1,25 @@
-from program_catalog.programs.cambodia_rainfall import RainfallDerivative
-from program_catalog.programs.cuban_blizzard import CriticalSnowfallDerivative
-
-
-_PROGRAM_DIRECTORY = {
-    'rainfall_derivative': {
-        'names': ['GRP', 'XSR'],
-        'program': RainfallDerivative
-    },
-    'critical_snowfall_derivative': {
-        'names': ['CriticalSnowday'],
-        'program': CriticalSnowfallDerivative
-    }
-}
+from program_catalog.programs.rainfall_derivative import RainfallDerivative
+from program_catalog.programs.critical_snowfall_derivative import CriticalSnowfallDerivative
 
 
 def get_program(program_name):
     ''' Maps program names to Program classes
 
-        Parameters: program name (str), name of the program
+        Parameters: program_name (str), name of the program
         Returns: Program, class pointer for desired program
-        or None if program name is improperly specified
+        or None if program_name is improperly specified
     '''
-    for program in _PROGRAM_DIRECTORY:
-        if program_name in _PROGRAM_DIRECTORY[program]['names']:
-            return _PROGRAM_DIRECTORY[program]['program']
+    program_directory = {
+    'rainfall_derivative': {
+        'names': ['GRP', 'XSR'],
+        'program': RainfallDerivative
+        },
+    'critical_snowfall_derivative': {
+        'names': ['CriticalSnowday'],
+        'program': CriticalSnowfallDerivative
+        }
+    }
+    for program_key in program_directory:
+        if program_name in program_directory[program_key]['names']:
+            return program_directory[program_key]['program']
     return None

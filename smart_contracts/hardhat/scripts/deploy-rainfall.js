f1726e36f17d9f9fbb09f17a4aa19d0348f61f8d
Daniel Parangi <dmp267@cornell.edu>
2021-12-23 17:22:38 -0500

    reorg

--- smart_contracts/hardhat/scripts/deploy-rainfall.js
+++ smart_contracts/hardhat/scripts/deploy-rainfall.js
@@ -1,7 +1,8 @@
 const hre = require("hardhat");
-const RainfallSRO1 = require("../SROs/Cambodia_rain_basket_August21-December21.json");
+var RainfallSRO1 = require("../SROs/Cambodia_rain_basket_August21-December21.json");
 const RainfallSRO2 = require("../SROs/Cambodia_rain_basket_December21-April22.json");
-const DeployedContracts = require(process.cwd()+"/logs/deployments.json");
+var Providers = require(process.cwd()+"/logs/providers.json");
+var Contracts = require(process.cwd()+"/logs/contracts.json");
 const fs = require("fs");
 
 async function main() {
@@ -9,18 +10,18 @@ async function main() {
   const RainfallDerivativeProvider = await hre.ethers.getContractFactory("RainfallDerivativeProvider");
   var derivative_provider = null;
 
-  if ("RainfallDerivativeProvider" in DeployedContracts) {
+  if ("RainfallDerivativeProvider" in Providers) {
     provider_address = DeployedContracts.RainfallDerivativeProvider;
     derivative_provider = await RainfallDerivativeProvider.attach(provider_address);
     console.log("RainfallDerivativeProvider already deployed to:", provider_address);
-    
   } else {
     derivative_provider = await RainfallDerivativeProvider.deploy();
     await derivative_provider.deployed();
-    console.log("RainfallDerivativeProvider deployed to:", derivative_provider.address);
-
-    DeployedContracts["RainfallDerivativeProvider"] = derivative_provider.address
-    var deployment_content = JSON.stringify(DeployedContracts);
+    var address = derivative_provider.address;
+    console.log("RainfallDerivativeProvider deployed to:", address);
+    Providers["RainfallDerivativeProvider"] = address;
+    var deployment_content = JSON.stringify(Providers);
+    console.log(deployment_content);
     fs.writeFile(process.cwd()+"/logs/deployments.json", deployment_content, "utf8", function (err) {
       if (err) {
         console.log("error writing to json");
@@ -29,6 +30,7 @@ async function main() {
     });
   }
 
+  RainfallSRO1.__config__.contracts.push(RainfallSRO2);
   for (const contract of RainfallSRO1.__config__.contracts) {
     var id = contract.__config__.id.toString();
     if (id in DeployedContracts) {
@@ -62,60 +64,18 @@ async function main() {
       rainfall_option = await RainfallOption.attach(deployed_address);
       console.log("RainfallOption deployed to:", deployed_address);
 
-      DeployedContracts[id] = deployed_address;
-      var deployment_content = JSON.stringify(DeployedContracts);
-      fs.writeFile(process.cwd()+"/logs/deployments.json", deployment_content, "utf8", function (err) {
-        if (err) {
-          console.log("error writing to json");
-          return console.log(err)
-        }
-      });
+      Contracts[id] = deployed_address;
     }
   }
 
-  for (const contract of RainfallSRO2.__config__.contracts) {
-    var id = contract.__config__.id.toString();
-    if (id in DeployedContracts) {
-      console.log("RainfallOption already deployed to:", DeployedContracts[id]);
-      continue;
-    } else {
-      var opt_type = contract.__config__.payouts.__config__.derivative.__config__.opt_type.toString();
-      var dataset = contract.__config__.payouts.__config__.index_distribution.__config__.index.__config__.loader.__config__.dataset_name.toString();
-      var strike = contract.__config__.payouts.__config__.derivative.__config__.strike;
-      var limit = contract.__config__.payouts.__config__.derivative.__config__.limit;
-      var tick = contract.__config__.payouts.__config__.derivative.__config__.tick;
-      var start_date = new Date(contract.__config__.payouts.__config__.index_distribution.__config__.index.__config__.start);
-      var start = parseInt(start_date.getTime() / 1000);
-      var end_date = new Date(contract.__config__.payouts.__config__.index_distribution.__config__.index.__config__.end);
-      var end = parseInt(end_date.getTime() / 1000);
-      var parameters = [id, dataset, opt_type, start.toString(), end.toString(), strike.toString(), limit.toString(), tick.toString()]
-      var locations = [];
-      for (const config of contract.__config__.payouts.__config__.index_distribution.__config__.index.__config__.loader.__config__.loaders) {
-        var lat = config.__config__.lat;
-        var lon = config.__config__.lon;
-        var location_str = "[" + lat.toString() + ", " + lon.toString() + "]";
-        locations.push(location_str);
-      }
-
-      var tx = await derivative_provider.newContract(locations, parameters, end);
-      var receipt = await tx.wait();
-      console.log(receipt.logs)
-
-      var deployed_address = await derivative_provider.getContractAddress(id);
-      const RainfallOption = await hre.ethers.getContractFactory("RainfallOption");
-      rainfall_option = await RainfallOption.attach(deployed_address);
-      console.log("RainfallOption deployed to:", deployed_address);
-
-      DeployedContracts[id] = deployed_address;
-      var deployment_content = JSON.stringify(DeployedContracts);
-      fs.writeFile(process.cwd()+"/logs/deployments.json", deployment_content, "utf8", function (err) {
-        if (err) {
-          console.log("error writing to json");
-          return console.log(err)
-        }
-      });
+  var final_content = JSON.stringify(Contracts);
+  console.log(final_content);
+  fs.writeFile(process.cwd()+"/logs/deployments.json", final_content, "utf8", function (err) {
+    if (err) {
+      console.log("error writing to json");
+      return console.log(err)
     }
-  }
+  });
 }
 
 main()
--- smart_contracts/hardhat/scripts/verify-rainfall.js
+++ smart_contracts/hardhat/scripts/verify-rainfall.js
@@ -1,9 +1,21 @@
 const hre = require("hardhat");
-const DeployedContracts = require(process.cwd()+"/logs/deployments.json");
+const Providers = require(process.cwd()+"/logs/providers.json");
+const Contracts = require(process.cwd()+"/logs/contracts.json");
 
 async function main() {
 
-  for (const [name, address] of Object.entries(DeployedContracts)) {
+  for (const [name, address] of Object.entries(Providers)) {
+    try {
+      await hre.run("verify:verify", {
+        address: address,
+      });
+      console.log(name.toString() + " source code verified");
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  for (const [name, address] of Object.entries(Contracts)) {
     try {
       await hre.run("verify:verify", {
         address: address,

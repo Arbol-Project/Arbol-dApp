e3d7af6aed90315dde2c9fc217eeeebe057df482
Dan Parangi <dmp267@cornell.edu>
2021-12-28 16:56:31 -0500

    small change to adapter entrypoint

--- chainlink_node/adapter/entrypoint.sh
+++ chainlink_node/adapter/entrypoint.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 # python3 tests/test_adapter.py
-gunicorn --worker-class gevent --workers 2 --bind 172.17.0.1:8000 wsgi:app --log-level info
+#--workers 2
+gunicorn --worker-class gevent --bind 172.17.0.1:8000 wsgi:app --log-level info

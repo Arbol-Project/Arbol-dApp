e43d580f485e7051191b47b1f2d23a0bf5ba6397
Dan Parangi <dmp267@cornell.edu>
2021-12-28 15:01:24 -0500

    update receive secrets script

--- chainlink_node/scripts/receive-secrets.sh
+++ chainlink_node/scripts/receive-secrets.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-unzip ~/Arbol-dApp/chainlink_node/chainlink/secrets.zip
-unzip ~/Arbol-dApp/chainlink_node/adapter/tests/SROs.zip
+unzip ~/Arbol-dApp/chainlink_node/chainlink/secrets.zip -d ~/Arbol-dApp/chainlink_node/chainlink/
+unzip ~/Arbol-dApp/chainlink_node/adapter/tests/SROs.zip -d ~/Arbol-dApp/adapter/tests/

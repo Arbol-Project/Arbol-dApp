d0b6e97e10690dbebefc697bafbd6d90b274e81b
Dan Parangi <dmp267@cornell.edu>
2021-12-29 09:05:01 -0500

    run tests on node setup to preload IPFS data

--- chainlink_node/ipfs/entrypoint.sh
+++ chainlink_node/ipfs/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 ipfs init
-ipfs bootstrap rm --all
+# ipfs bootstrap rm --all
 ipfs bootstrap add '/ip4/198.211.104.50/tcp/4001/p2p/QmWsAFSDajELyneR7LkMsgfaRk2ib1y3SEU7nQuXSNPsQV'
 ipfs daemon

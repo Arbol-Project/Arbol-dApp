46415a0b4130731081d097044de2eb600990b5cf
Dan Parangi <dmp267@cornell.edu>
2021-12-28 22:46:13 -0500

    fixing ipfs docker

--- chainlink_node/adapter/entrypoint.sh
+++ chainlink_node/adapter/entrypoint.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-# python3 tests/test_adapter.py
-gunicorn --worker-class gevent --workers 2 --bind 172.17.0.1:8000 wsgi:app --log-level info
+python3 tests/test_adapter.py
+#--workers 5
+gunicorn --worker-class gevent --bind 172.17.0.1:8000 wsgi:app --log-level info

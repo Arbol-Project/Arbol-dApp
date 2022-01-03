0967fb281fac31af5f508a425878de7932bf95d0
Daniel Parangi <dmp267@cornell.edu>
2022-01-02 20:10:16 -0500

    remove extra space

--- smart_contracts/hardhat/scripts/evaluate-deployments.js
+++ smart_contracts/hardhat/scripts/evaluate-deployments.js
@@ -5,7 +5,6 @@ const ProviderLogs = path.join(process.cwd(), "../../web_app/packages/contracts/
 const ContractLogs = path.join(process.cwd(), "../../web_app/packages/contracts/src/logs/contracts.json");
 const Providers = require(ProviderLogs);
 const Contracts = require(ContractLogs);
-
 const delay = ms => new Promise(res => setTimeout(res, ms));
 
 

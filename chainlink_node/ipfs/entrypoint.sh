face4b405003014437b5f68c5100c65cb6e14c53
Dan Parangi <dmp267@cornell.edu>
2022-03-10 11:35:41 -0500

    attempt fix: change ipfs API address

--- chainlink_node/ipfs/entrypoint.sh
+++ chainlink_node/ipfs/entrypoint.sh
@@ -3,4 +3,5 @@
 ipfs init
 ipfs bootstrap rm --all
 ipfs bootstrap add '/ip4/198.211.104.50/tcp/4001/p2p/QmWsAFSDajELyneR7LkMsgfaRk2ib1y3SEU7nQuXSNPsQV'
+ipfs config Addresses.API /ip4/0.0.0.0/tcp/5001
 ipfs daemon

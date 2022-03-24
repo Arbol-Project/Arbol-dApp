9434444e5c1bf965063a9c5c9c81da6c0750d998
Dan Parangi <dmp267@cornell.edu>
2022-03-24 18:25:31 -0400

    testing

--- chainlink_node/adapter/app.py
+++ chainlink_node/adapter/app.py
@@ -30,7 +30,6 @@ def build_app():
         if data == '':
             data = {}
         response = API(data)
-        print(response.result['result']['data'])
         return jsonify(response.result)
 
     @app.route('/health', methods=['POST'])
